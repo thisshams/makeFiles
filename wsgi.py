06bf0c42d10104a20cdc2e9107467a7dc31af23c
thisshams <thisshams@gmail.com>
2022-10-18 20:08:05 +0530

    vercel change

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os,sys
 sys.path.append(os.apth.abspath(os.path.join(os.apth.dirname(__file__),"..")))
 from main import app
-app=app()
+myapp=app()
 if __name__=="__main__":
-    app.run()
+    myapp.run()
